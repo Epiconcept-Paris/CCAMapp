31fb7265d484609fd0267bbdd8407aa5b20630ba
Yohann Mansiaux <y.mansiaux@epiconcept.fr>
2025-12-05 09:32:20 +0100

    feat: use retrieve_ccam_thematics in mod_thematics

--- R/mod_thematics.R
+++ R/mod_thematics.R
@@ -44,52 +44,9 @@ mod_thematics_server <- function(id, rv, all_thematics_codes) {
     })
 
     observeEvent(rv$ccam, {
-      ccam_with_thematics <- lapply(
-        seq_len(length(all_thematics_codes)),
-        function(x) {
-          if (any(rv$ccam %in% all_thematics_codes[[x]])) {
-            ccam_present_in_thematique <- intersect(
-              rv$ccam,
-              all_thematics_codes[[x]]
-            )
-            list(
-              ccam_present_in_thematique = ccam_present_in_thematique,
-              thematique = names(all_thematics_codes)[x]
-            )
-          } else {
-            return(NULL)
-          }
-        }
-      )
-      ccam_with_thematics <- data.table::rbindlist(ccam_with_thematics)
-      selected_ccam_with_thematics <- data.table::data.table(code = rv$ccam)
-      if (nrow(ccam_with_thematics) > 0) {
-        selected_ccam_with_thematics <- merge(
-          selected_ccam_with_thematics,
-          ccam_with_thematics,
-          by.x = "code",
-          by.y = "ccam_present_in_thematique",
-          all.x = TRUE
-        )
-      } else {
-        selected_ccam_with_thematics <- data.table::data.table(
-          code = rv$ccam,
-          thematique = "Thématique inconnue"
-        )
-      }
-      selected_ccam_with_thematics <- selected_ccam_with_thematics[
-        is.na(thematique),
-        thematique := "Thématique inconnue"
-      ]
-      thematique_levels <- unique(selected_ccam_with_thematics$thematique)
-      thematique_levels <- c(
-        "Thématique inconnue",
-        setdiff(thematique_levels, "Thématique inconnue")
-      )
-      selected_ccam_with_thematics$thematique <- factor(
-        selected_ccam_with_thematics$thematique,
-        levels = thematique_levels
-      )
+        
+        selected_ccam_with_thematics <- retrieve_ccam_thematics(rv$ccam, all_thematics_codes)
+
       local_rv$selected_ccam_with_thematics <- selected_ccam_with_thematics[order(
         thematique
       )]
